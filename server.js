168e9d825b4bb9534e6ac943d8b1c0401f5ebb90
Adam Nelip <Adam.Nelip@gmail.com>
2018-08-01 17:45:13 +0200

    Working Files

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.get('/userform', function (req, res) {
     const response = {
         first_name: req.query.first_name,
         last_name: req.query.last_name,
-        image: req.query.image
     };
     res.end(JSON.stringify(response));
 });
